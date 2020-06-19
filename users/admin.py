0a1fcd930f4c0422949f67ebc3556dbafb61ae18
bigeye93 <jyj9312@naver.com>
2020-06-20 00:33:45 +0900

    add users app

---
+++ users/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
+
+
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+    """ Custom User Admin """
+
+    list_display = ("username", "email", "coe", "part", "superuser")
+    list_filter = ("coe", "part")
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {"fields": ("avatar", "empoyeeNumber", "coe", "part", "superuser",)},
+        ),
+    )
---
+++ users/models.py
@@ -0,0 +1,51 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    """ Custom User Model """
+
+    # GENDER_MALE = "male"
+    # GENDER_FEMALE = "female"
+    # GENDER_OTHER = "other"
+
+    # GENDER_CHOICES = (
+    #     (GENDER_MALE, "Male"),
+    #     (GENDER_FEMALE, "Female"),
+    #     (GENDER_OTHER, "Other"),
+    # )
+
+    # avatar = models.ImageField(null=True, blank=True)
+    # gender = models.CharField(
+    #     choices=GENDER_CHOICES, max_length=10, null=True, blank=True
+    # )
+    # bio = models.TextField(default="", blank=True)
+
+    COE_ESSD = "eSSD PV"
+    COE_CSSD = "cSSD PV"
+
+    COE_CHOICES = (
+        (COE_ESSD, "eSSD PV"),
+        (COE_CSSD, "cSSD PV"),
+    )
+
+    PART_FTL = "FTL"
+    PART_HIL = "HIL"
+    PART_PVT = "PVT"
+    PART_MANAGER = "Manager"
+    PART_OTHER = "Other"
+
+    PART_CHOICES = (
+        (PART_FTL, "FTL"),
+        (PART_HIL, "HIL"),
+        (PART_PVT, "PVT"),
+        (PART_MANAGER, "Manager"),
+        (PART_OTHER, "Other"),
+    )
+
+    avatar = models.ImageField(blank=True)
+    empoyeeNumber = models.IntegerField(null=True, blank=True)
+    coe = models.CharField(choices=COE_CHOICES, max_length=10, blank=True)
+    part = models.CharField(choices=PART_CHOICES, max_length=10, blank=True)
+    superuser = models.BooleanField(default=False)
+    # project
