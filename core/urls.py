e9a7e834bf25d38d5c3ea159c18a5ea7cd486888
bigeye93 <jyj9312@naver.com>
2020-06-22 22:49:56 +0900

    initial view works

---
+++ core/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from products import views as product_views
+
+app_name = "core"
+urlpatterns = [path("", product_views.all_products, name="home")]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from . import models
 
-# Create your views here.
+# from django.http import HttpResponse
+
+
+def all_products(request):
+    # return HttpResponse(content="hello")
+    all_products = models.Product.objects.all()
+    return render(request, "products/home.html", context={"products": all_products,})
